b2f8c2b3687c4b63050d6e09f765ca8be4499515
Manuel Silva <manuelt226@gmail.com>
2025-05-31 00:15:57 +0000

    y CL en el logo que sea L33

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -14,7 +14,7 @@ export function CorreoLibroLogo(props: SVGProps<SVGSVGElement>) {
       <path d="M25 15 H35 V85 H25z" />
       <path d="M40 20 Q50 10 60 20 V80 Q50 90 40 80z" />
       <path d="M65 15 H75 V85 H65z" />
-      <text x="50" y="55" fontFamily="Roboto, sans-serif" fontSize="12" textAnchor="middle" fill="hsl(var(--background))" className="font-headline">CL</text>
+      <text x="50" y="55" fontFamily="Roboto, sans-serif" fontSize="12" textAnchor="middle" fill="hsl(var(--background))" className="font-headline">L33</text>
     </svg>
   );
 }
